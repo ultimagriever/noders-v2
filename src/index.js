d2442d894221773d6c3f50d3d37fbfaf71a24bfc
Pamela Argentino <pamela.toptal@rappi.com>
2020-08-14 16:39:39 -0300

    Add database connectivity

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Express server listening to requests on port %d', port));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected succesfully to MongoDB.');
+    app.listen(port, () => console.log('Express server listening to requests on port %d', port))
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
