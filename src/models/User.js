f4db7ba0795a5f9e86c117f7c12f216572cbf649
Pamela Argentino <pamela.toptal@rappi.com>
2020-08-14 17:17:49 -0300

    Create User model

---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+
+    callback(null, isMatch);
+  });
+};
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
