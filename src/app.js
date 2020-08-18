b040a495c8a30d6cdac045a1bc36dbe1d60e998b
Pamela Argentino <pamela.toptal@rappi.com>
2020-08-18 04:13:07 -0300

    Add service update step to codeship-steps.yml

--- src/app.js
+++ src/app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/todos', todoRouter);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World from AWS ECS-Fargate!'));
 
 module.exports = app;
